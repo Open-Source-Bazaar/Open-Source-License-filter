4430edbb3f6bc51c105a8c47838a9b7dd744d162
luojiyin <luojiyin@hotmail.com>
2022-12-22 21:25:27 +0800

    fix typo error

--- source/data.ts
+++ source/data.ts
@@ -615,7 +615,7 @@ export const licenses: License[] =[
     },
     {
       key: 'RPL1.5',
-      name: 'Reciprocal Public License FeatureAttitude.Positive.5',
+      name: 'Reciprocal Public License 1.5',
       link: 'https://spdx.org/licenses/RPL-1.5.html',
       feature: {
         popularity: FeatureAttitude.Negative,
